b453020010e521f19d4c93939f36c5cb19f9c38a
Daeyong96 <57346424+Daeyong96@users.noreply.github.com>
2019-11-25 03:50:34 +0900

    account, student,admin
    add

---
+++ Administrator.java
@@ -0,0 +1,30 @@
+//package Teamproj;
+
+public class Administrator {
+	private String name;
+	private String adminNum;
+	private String job = "Admin";
+	
+	public void getAdminInfo() {
+		
+	}
+	public String getJob() {
+		return job;
+	}
+	//get set
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdminNum() {
+		return adminNum;
+	}
+	public void setAdminNum(String adminNum) {
+		this.adminNum = adminNum;
+	}
+	//get set
+	}
+	
+}
---
+++ Student.java
@@ -0,0 +1,56 @@
+//package Teamproj;
+
+public class Student {
+	private String name; //성명
+	private String studentNum; //학번
+	private String major; //학과
+	private String track; //트랙
+	private int country; //국적 1한국, 0다른나라
+	private String job = "Student";
+	
+	//get set
+	
+	public String getJob() {
+		return job;
+	}
+	
+	public void getStudentInfo() {
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getStudentNum() {
+		return studentNum;
+	}
+	public void setStudentNum(String studentnum) {
+		this.studentNum = studentnum;
+	}
+	
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	
+	public String getTrack(){
+		return track;
+	}
+	public void setTrack(String track) {
+		this.track = track;
+	}
+	
+	public int getCountry() {
+		return country;
+	}
+	public void setCountry(int country) {
+		this.country = country;
+	}
+	
+	//get set 
+}
