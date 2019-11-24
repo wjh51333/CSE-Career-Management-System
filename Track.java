b96d3b888de7fa8b337634acf3bcaa04a04c665d
cwc06129 <45617707+cwc06129@users.noreply.github.com>
2019-11-24 22:11:54 +0900

    Create Track.java

---
+++ Track.java
@@ -0,0 +1,24 @@
+package software_design;
+/*
+ * 심화컴퓨터 전공의 경우, 1
+ * 글솦의 경우, 2
+ * 연계/융햡전공의 경우, 3
+ */
+
+public class Track 
+{
+	public void print_detailed_graduation_requirement(int track_number)
+	{
+		
+	}
+	
+	public void print_gradution_requirement(int track_number)
+	{
+		
+	}
+	
+	public void print_curriculum(int track_number)
+	{
+		
+	}
+}
