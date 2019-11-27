9e3b87c5f8c9598f381de58b383d85020ac0ea07
njhw20 <50469773+njhw20@users.noreply.github.com>
2019-11-28 00:41:26 +0900

    Update CompleteList.java

--- CompleteList.java
+++ CompleteList.java
@@ -10,6 +10,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+
 //이수 목록
 //재이수 가능 판단, 점수 계산, 필수과목수강확인, 강의추가, 강의 삭제
 public class CompleteList {
@@ -25,439 +26,125 @@ public class CompleteList {
 	public int getTotalgrade() {
 		return totalgrade;
 	}
-
-
-
 	public void setTotalgrade(int totalgrade) {
 		this.totalgrade = totalgrade;
 	}
-
-
-
 	public int getMajorgrade() {
 		return majorgrade;
 	}
-
-
-
 	public void setMajorgrade(int majorgrade) {
 		this.majorgrade = majorgrade;
 	}
-
-
-
 	public int getRefinegrade() {
 		return refinegrade;
 	}
-
-
-
 	public void setRefinegrade(int refinegrade) {
 		this.refinegrade = refinegrade;
 	}
-
-
-
 	public int getBasicrefine() {
 		return basicrefine;
 	}
-
-
-
 	public void setBasicrefine(int basicrefine) {
 		this.basicrefine = basicrefine;
 	}
-
-
-
 	public int getMajorbase() {
 		return majorbase;
 	}
-
-
-
 	public void setMajorbase(int majorbase) {
 		this.majorbase = majorbase;
 	}
-
-
-
 	public int getEnginmajor() {
 		return enginmajor;
 	}
-
-
-
 	public void setEnginmajor(int enginmajor) {
 		this.enginmajor = enginmajor;
 	}
-
-
-
+	CourseGrade c=new CourseGrade();
 	public int getYearsemester() {
-		return yearsemester;
+		return c.yearsemester;
 	}
-
-
-
 	public void setYearsemester(int yearsemester) {
-		this.yearsemester = yearsemester;
+		this.c.yearsemester = yearsemester;
 	}
-
-
-
 	public String getChecktake() {
-		return checktake;
+		return c.checktake;
 	}
-
-
-
 	public void setChecktake(String checktake) {
-		this.checktake = checktake;
+		this.c.checktake = checktake;
 	}
-
-
-
 	public String getScore() {
-		return score;
+		return c.score;
 	}
-
-
-
 	public void setScore(String score) {
-		this.score = score;
+		this.c.score = score;
 	}
-
-
-
+	CourseInfo i=new CourseInfo();
 	public String getCoursename() {
-		return coursename;
+		return i.coursename;
 	}
-
-
-
 	public void setCoursename(String coursename) {
-		this.coursename = coursename;
+		this.i.coursename = coursename;
 	}
-
-
-
 	public String getCoursecode() {
-		return coursecode;
+		return i.coursecode;
 	}
-
-
-
 	public void setCoursecode(String coursecode) {
-		this.coursecode = coursecode;
+		this.i.coursecode = coursecode;
 	}
-
-
-
 	public String getGrade() {
-		return grade;
+		return i.grade;
 	}
-
-
-
 	public void setGrade(String grade) {
-		this.grade = grade;
+		this.i.grade = grade;
 	}
 	
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String filename;
-		CompleteList c=new CompleteList();
-		filename=sc.next();
-		c.calgrade(filename);
-	}
-
-
 	public void calgrade(String filename) {
 		
-		 
-        int count=0, i;
-        String arr[]=new String[6000000];
         String filename2=filename.split(".txt")[0];
-		try {
-			Scanner scan = new Scanner(new FileReader(new File(filename)));
-		        while(scan.hasNext()){
-		        	arr[count]=scan.next();
-		            count++;
-		        }
-		        
-				File file=new File(filename2+"공학전공.txt");
-				file.delete();
-				File file2=new File(filename2+"기본소양.txt");
-				file2.delete();
-				File file3=new File(filename2+"전공기반.txt");
-				file3.delete();
-				File file4=new File(filename2+"교양.txt");
-				file4.delete();
-				
-		        for(i=0; i<count; i++) {
-		        		if (arr[i].equals("공학전공")) {
-		        			try {
-		        				PrintWriter outputStream = new PrintWriter(new FileWriter(filename2+"공학전공.txt", true));
-		        				outputStream.println(arr[i+4]);
-		        				outputStream.close();
-		        			} catch (IOException e) {
-		        				System.out.println("오류 발생! 관리자에게 문의해주세요");
-		        				System.exit(0);
-		        			}
-		        		}
-		        		if (arr[i].equals("기본소양")) {
-		        			try {
+        int i,count=0;
+        
+        Data d=new Data();
+		String [] arr=new String[150];
+		for (i=0; i<150;i++)
+			arr[i]=null;
 
-		        				PrintWriter outputStream2 = new PrintWriter(new FileWriter(filename2+"기본소양.txt", true));
-		        				outputStream2.println(arr[i+4]);
-		        				outputStream2.close();
-		        			} catch (IOException e) {
-		        				System.out.println("오류 발생! 관리자에게 문의해주세요");
-		        				System.exit(0);
-		        			}
-		        		}
-		        		if (arr[i].equals("전공기반")) {
-		        			try {
-		        				PrintWriter outputStream3 = new PrintWriter(new FileWriter(filename2+"전공기반.txt", true));
-		        				outputStream3.println(arr[i+4]);
-		        				outputStream3.close();
-		        			} catch (IOException e) {
-		        				System.out.println("오류 발생! 관리자에게 문의해주세요");
-		        				System.exit(0);
-		        			}
-		        		}
-		        		if (arr[i].equals("교양")) {
-		        			try {
+        arr=d.getfile2(filename);
 
-		        				
-		        				PrintWriter outputStream4 = new PrintWriter(new FileWriter(filename2+"교양.txt", true));
-		        				outputStream4.println(arr[i+4]);
-		        				outputStream4.close();
-		        			} catch (IOException e) {
-		        				System.out.println("오류 발생! 관리자에게 문의해주세요");
-		        				System.exit(0);
-		        			}
-		        		}
-		        		
-		        	}
-		        
-		        scan.close();
-
-		    
-		} catch (FileNotFoundException e) {
-			System.out.println("오류 발생! 관리자에게 문의해주세요");
-			System.exit(0);
-		}
+        d.mdfile(filename2+"공학전공.txt");
+        d.mdfile(filename2+"기본소양.txt");
+        d.mdfile(filename2+"교양.txt");
+        d.mdfile(filename2+"전공기반.txt");
+        
+        for (i=0; i<150;i++)
+        	if (arr[i]==null) {
+        		count=i;
+        		break;
+        	}
 		
-		int next;
-		int sum=0;
-		Scanner inputStream=null;
-        try {
-            inputStream = new Scanner(new FileInputStream(filename2+"공학전공.txt"));
-
-    		sum=0;
-    		while (inputStream.hasNextInt()) {
-    			next = inputStream.nextInt( );
-    			enginmajor+=next;
-    	    }
-    		inputStream.close();
-        }
-        catch(FileNotFoundException e)
-        {
-            System.out.println("오류 발생! 관리자에게 문의해주세요");
-            System.exit(0);
-        }
-		
-	
-
-		
-		Scanner inputStream2=null;
-		try {
-            inputStream2 = new Scanner(new FileInputStream(filename2+"전공기반.txt"));
-    		sum=0;
-    		
-    		while (inputStream2.hasNextInt()) {
-    			next = inputStream2.nextInt( );
-    			majorbase+=next;
-    	    }
-    		
-    		inputStream2.close();
-        }
-        catch(FileNotFoundException e)
-        {
-            System.out.println("오류 발생! 관리자에게 문의해주세요");
-            System.exit(0);
-        }
-		
-
-		
-		Scanner inputStream3=null;
-		try {
-            inputStream3 = new Scanner(new FileInputStream(filename2+"기본소양.txt"));
-    		sum=0;
-    		
-    		while (inputStream3.hasNextInt()) {
-    			next = inputStream3.nextInt( );
-    			basicrefine+=next;
-    	    }
-    		
-    		inputStream3.close();
-        }
-        catch(FileNotFoundException e)
-        {
-            System.out.println("오류 발생! 관리자에게 문의해주세요");
-            System.exit(0);
-        }
-		
-
-		
-		Scanner inputStream4=null;
-		try {
-            inputStream4 = new Scanner(new FileInputStream(filename2+"교양.txt"));
-    		sum=0;
-    		
-    		while (inputStream4.hasNextInt()) {
-    			next = inputStream4.nextInt( );
-    			refinegrade+=next;
-    	    }
-    		
-    		inputStream4.close();
-        }
-        catch(FileNotFoundException e)
-        {
-            System.out.println("오류 발생! 관리자에게 문의해주세요");
-            System.exit(0);
+        for(i=0; i<count; i++) {
+        		if (arr[i].equals("공학전공")) {
+        			d.setfile(filename2+"공학전공.txt", arr[i+4]);
+        		}
+        		if (arr[i].equals("기본소양")) {
+        			d.setfile(filename2+"기본소양.txt", arr[i+4]);
+        		}
+        		if (arr[i].equals("전공기반")) {
+        			d.setfile(filename2+"전공기반.txt", arr[i+4]);
+        		}
+        		if (arr[i].equals("교양")) {
+        			d.setfile(filename2+"교양.txt", arr[i+5]);
+        		}
+        		if (arr[i]==null)
+        			break;
         }
 
-
+		enginmajor=d.getfile3(filename2+"공학전공.txt");
+		majorbase=d.getfile3(filename2+"전공기반.txt");
+		basicrefine=d.getfile3(filename2+"기본소양.txt");
+		refinegrade=d.getfile3(filename2+"교양.txt");
 		
 		majorgrade=basicrefine+majorbase+enginmajor;
 		totalgrade=majorgrade+refinegrade;
-		
-		
+	
 	}
-	
-
-	
-	private int yearsemester;
-	private String checktake;
-	private String score;
-	private String coursename;
-	private String coursecode;
-	private String grade;
-
-	public void addcourse(String filename) {
-		
-		CourseGrade cg= new CourseGrade();
-		CourseInfo ci=new CourseInfo();
-		
-		System.out.println("년도/학기");
-		Scanner keyboard = new Scanner(System.in);
-		yearsemester=cg.yearsemester;
-		yearsemester = keyboard.nextInt();
-
-
-		Scanner keyboard2 = new Scanner(System.in);
-		System.out.println("교과구분");			
-		checktake=cg.checktake;
-		checktake = keyboard2.nextLine();
-	
-		System.out.println("과목명");
-		coursename=ci.coursename;
-		coursename = keyboard2.nextLine();
-		
-		System.out.println("과목코드");
-		coursecode=ci.coursecode;
-		coursecode = keyboard2.nextLine();
-		
-		System.out.println("학점");
-		grade=ci.grade;
-		grade = keyboard2.nextLine();
-		
-		System.out.println("점수");
-		Scanner keyboard3=new Scanner(System.in);
-		score=cg.score;
-		score = keyboard3.nextLine();
-		
-
-
-        try  {
-        	PrintWriter outputStream = new PrintWriter(new FileWriter(filename, true));
-        	outputStream.print(yearsemester+" "+ checktake+" "+score+" "+coursename + " " + coursecode + " " + grade);
-        	outputStream.println(" ");
-        	outputStream.close();
-        }
-        catch(FileNotFoundException e) {
-            System.out.println("오류 발생! 관리자에게 문의해주세요");
-            System.exit(0);
-        }
-        catch (IOException e) {
-        	System.out.println("오류 발생! 관리자에게 문의해주세요");
-            System.exit(0);
-		}
-	}
-	
-
-
-
-	public void delcourse(String filename) {
-		
-		String line=null;
-		String []arr=new String[1000000];
-		int count=0;
-		int del_line;
-		BufferedReader br;
-		try {
-			br = new BufferedReader(new FileReader(filename));
-			while((line=br.readLine())!=null) {
-				arr[count]=line;
-				count++;
-			}
-			
-			
-			
-			br.close();
-			
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		File file=new File(filename);
-		file.delete();
-		
-		int i;
-
-		Scanner sc=new Scanner(System.in);
-		System.out.println("삭제할 행 번호는? ");
-		del_line=sc.nextInt();
-		
-		try  {
-        	PrintWriter outputStream = new PrintWriter(new FileWriter(filename, true));
-        	for (i=0; i<del_line; i++) {
-        		outputStream.println(arr[i]);
-        	}
-        	for(i=del_line+1; i<count; i++) {
-        		outputStream.println(arr[i]);
-        	}
-        	outputStream.close();
-        }
-		
-		catch(FileNotFoundException e) {
-            System.out.println("오류 발생! 관리자에게 문의해주세요");
-            System.exit(0);
-        } catch (IOException e) {
-			
-			e.printStackTrace();
-		}
-		
-
-	}
-	
-
 }
